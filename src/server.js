3fff38d59cb70ec35a3d2caf4447afeeb6c882d4
Puspendra Kumar Baghel <138773936+puspendra123b@users.noreply.github.com>
2026-06-16 19:32:40 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV !== "production") {
       const server = http.createServer(app);
 
       initWebSocket(server);
-      await connectRedis();
+      // await connectRedis();
 
       server.listen(PORT, () => {
         console.log(`🚀 Server running on port ${PORT}`);
